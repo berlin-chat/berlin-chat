c5ff6e322644d0c6ebf3a248f50bcfbe5a2a9cfb
Thilo Ilg <contact@thiloilg.com>
2020-04-04 20:56:15 +0200

    feat(app): remove slash from prod api origin

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,3 +1,3 @@
 const isDev = process.env.NODE_ENV === 'development'
 
-export const API_ORIGIN: string = isDev ? 'http://localhost:8080' : 'https://api.berlin-chat.soon.it/'
+export const API_ORIGIN: string = isDev ? 'http://localhost:8080' : 'https://api.berlin-chat.soon.it'
