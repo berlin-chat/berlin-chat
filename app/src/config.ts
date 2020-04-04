47a4acff78b640f4e2b78cecf758310b4ae78fd0
Thilo Ilg <contact@thiloilg.com>
2020-04-04 18:48:22 +0200

    feat(app): change env vars on prod

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,1 +1,3 @@
-export const API_ORIGIN: string = process.env.API_ORIGIN || 'http://localhost:8080'
+const isDev = process.env.NODE_ENV === 'development'
+
+export const API_ORIGIN: string = isDev ? 'http://localhost:8080' : 'http://berlin-chat.soon.it:8080'
