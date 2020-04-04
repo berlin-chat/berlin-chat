dfcb41a75c009f961c5e4b68d7c9ad3e9234865f
Thilo Ilg <contact@thiloilg.com>
2020-04-04 19:43:43 +0200

    feat(all): add traefik reverse proxy

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,3 +1,3 @@
 const isDev = process.env.NODE_ENV === 'development'
 
-export const API_ORIGIN: string = isDev ? 'http://localhost:8080' : 'http://berlin-chat.soon.it:8080'
+export const API_ORIGIN: string = isDev ? 'http://localhost:8080' : 'http://api.berlin-chat.soon.it'
